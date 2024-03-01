f1b4d6aa1b5e8945e48f05d224d99848e136f384
Daniil Ulsky <daniil@spaceshub.net>
2024-03-01 15:59:10 +0600

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'project-name';
+  tasks: any;
+
+  constructor(
+    httpClient: HttpClient
+  ) {
+    httpClient.get('tasks').pipe(
+      map((tasks) => {
+        this.tasks = tasks;
+        return tasks;
+      })
+    );
+  }
 }
